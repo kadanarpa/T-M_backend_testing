a8438ff3fbcd6c4d0a20035396054ccc242485e3
daisy-dev <kadanarpa@gmail.com>
2024-10-16 04:16:51 -0500

    feat: migrate to Spring Security 6.x \n- add testing users \n- add testing http security views

--- src/main/java/co/edu/usco/TM/security/WebSecurityConfig.java
+++ src/main/java/co/edu/usco/TM/security/WebSecurityConfig.java
@@ -2,11 +2,14 @@ package co.edu.usco.TM.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,24 +19,38 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/append", "/delete/{id}", "/modify/{id}").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
-                
+
+        http.authorizeHttpRequests(request
+                -> request.requestMatchers("/").permitAll()
+                        .requestMatchers("/append", "/delete/{id}").hasRole("ADMIN")
+                        .requestMatchers("/modify/{id}").authenticated()
+        )
+                .formLogin(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withDefaultPasswordEncoder().username("daisy").password("1602").roles("ADMIN")
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        User.UserBuilder userBuilder = User.builder();
+
+        UserDetails user1 = userBuilder.username("daisy")
+                .password(passwordEncoder.encode("2904"))
+                .roles("ADMIN")
                 .build();
-        UserDetails user2 = User.withDefaultPasswordEncoder().username("nicky").password("0406").roles("USER")
+
+        UserDetails user2 = userBuilder.username("nicky")
+                .password(passwordEncoder.encode("1902"))
+                .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
